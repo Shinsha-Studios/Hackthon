ad386245313320be68d01587f6ca8217f8424190
Loope <lukebuchanantx@gmail.com>
2022-11-12 18:48:52 -0600

    Updated app js and calculation js to integrate together better

--- Scripts/Calculation.js
+++ Scripts/Calculation.js
@@ -1,21 +1,6 @@
 // e fund = emergency fund
 // r fund = rainy day fund
 
-let eFundSlider = document.getElementById("emrgencyFundRange");
-let rFundSlider = document.getElementById("rainyDaySlider")
-let eFundVal = document.getElementById("emergencyFundRangeVal");
-let rFundVal = document.getElementById("ranyDayRange");
-eFundVal.innerHTML = eFundSlider.value;
-rFundVal.innerHTML = rFundSlider.value
-
-// event called when slider is moved
-eFundSlider.oninput = function() {
-  eFundVal.innerHTML = this.value;
-}
-rFundSlider.oninput = function() {
-    rFundVal.innerHTML = this.value;
-}
-
 function CalcFundCapacity(expenses, months) {
     return (expenses * months);
 }
@@ -25,35 +10,13 @@ function CalcNetEarnings(income, expenses) {
 }
 
 //Begin calculations (user has pressed calculate)
-function Calculate(income, expenses) {
-    let eFundCapacity = CalcFundCapacity(expenses, eFundSlider);
-    let rFundCapacity = CalcFundCapacity(expenses, rFundSlider);
-    
-    let netIncome = CalcNetEarnings(income, expenses);
-    let output = document.getElementById("netIncome");
-    output.innerHTML = netIncome;
-
+function CalculateMonths(capacity, netIncome) {
     let months = 0;
-    let runoff = 0;
     for(let i = eFundCapacity; i > 0; i -= netIncome) {
         months++;
-        i = runoff;                 //may have some money left after you fill up the fund so roll that money into the rainy day fund
     }
-    //it will take 'months' months to fill the emergency fund capacity of size 'efundcapacity'
-    output = document.getElementById("eFundCap");
-    output.innerHTML = eFundCapacity;
-    output = document.getElementById("eFundMonths");
-    output.innerHTML = months;
-
-    months = 0;
-    let i = rFundCapacity;
-    i += runoff;
-    for(i; i > 0; i -= netIncome) {
-        months++;
-    }
-    //it will take 'months' months to fill the rainy day fund capacity of size 'rfundcapacity'
-    output = document.getElementById("rFundCap");
-    output.innerHTML = rFundCapacity;
-    output = document.getElementById("rFundMonths");
-    output.innerHTML = months;
+    return months;
+}
+export{
+    CalculateMonths, CalcNetEarnings, CalcFundCapacity
 }
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import {
   CalcFundCapacity,
   CalcNetEarnings,
   Calculate,
+  CalculateMonths,
 } from "./Scripts/Calculation.js";
 
 import {
@@ -17,7 +18,24 @@ const monthlyIncomeInput = document.querySelector("#monthly-income");
 const monthlyExpenseInput = document.querySelector("#monthly-expense");
 const calcBttn = document.querySelector("#bttn-calc");
 
+eFundVal.innerHTML = eFundSlider.value;
+rFundVal.innerHTML = rFundSlider.value
+// event called when slider is moved
+eFundSlider.oninput = function() {
+  eFundVal.innerHTML = this.value;
+}
+rFundSlider.oninput = function() {
+    rFundVal.innerHTML = this.value;
+}
+
 calcBttn.addEventListener("click", () => {
   const monthlyExpense = monthlyExpenseInput.value;
   const monthlyIncome = monthlyIncomeInput.value;
+  const eFundCap = CalcFundCapacity(monthlyExpense, eFundVal);
+  const rFundCap = CalcFundCapacity(monthlyExpense, rFundVal);
+  const netIncome = CalcNetEarnings(monthlyIncome, monthlyExpense);
+  const eFundTime = CalculateMonths(eFundCap, netIncome);
+  const rfundTime = CalculateMonths(rFundCap, netIncome);
+
+
 });
