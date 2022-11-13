1d1561370652228e8d7cb42ee7d3c0ab1d6d54d0
ShifuX <91515578+ShifuX@users.noreply.github.com>
2022-11-13 10:14:36 -0600

    Added a chart that displays the data

--- Scripts/investment-calculations.js
+++ Scripts/investment-calculations.js
@@ -1,3 +1,5 @@
+const monthlyIncomeInput = document.querySelector("#monthly-income");
+const monthlyExpenseInput = document.querySelector("#monthly-expense");
 const annualRR = 0.08; // Average roi of the S&P 500
 
 // using the 4% rule, when investments can cover the monthly expense
@@ -9,6 +11,8 @@ function amtToRetire(totalExpense) {
 }
 
 // How long will it take to reach the amtToRetire
+const yrsData = [];
+const moneyData = [];
 function timeToInvest(contributionsPerMonth, totalExpense) {
   let totalAmt = amtToRetire(totalExpense);
   let yrs = 0;
@@ -19,12 +23,49 @@ function timeToInvest(contributionsPerMonth, totalExpense) {
       result += contributionsPerMonth * 12;
       result += result * annualRR;
       yrs++;
+      yrsData.push(yrs);
+      moneyData.push(result);
     } else {
       return -1;
     }
   }
-
+  makeChart();
   return yrs;
+}
+
+function makeChart() {
+  const ctx = document.querySelector("#Chart");
+  const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: yrsData,
+      datasets: [
+        {
+          label: "investment growth",
+          data: moneyData,
+          backgroundColor: ["#FCA311"],
+          borderColor: ["#14213D"],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+  monthlyIncomeInput.addEventListener("click", () => {
+    clearChart(myChart);
+  });
+  monthlyExpenseInput.addEventListener("click", () => {
+    clearChart(myChart);
+  });
+}
+
+function clearChart(myChart) {
+  myChart.destroy();
+  while (yrsData.length > 0) {
+    yrsData.pop();
+  }
+  while (moneyData.length > 0) {
+    moneyData.pop();
+  }
 }
 
 export { amtToRetire, timeToInvest };
