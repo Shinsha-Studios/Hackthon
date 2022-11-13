09eee684db1673ea35b6bbb3a3553ad53d43b046
Loope <lukebuchanantx@gmail.com>
2022-11-13 01:19:30 -0600

    added more input validation + units for output

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ import {
   timeToInvest,
 } from "./Scripts/investment-calculations.js";
 
-import { getStockData } from "./Scripts/stock.js";
-
 const eFundSlider = document.getElementById("emergencySlider");
 const rFundSlider = document.getElementById("rainySlider");
 const eFundVal = document.getElementById("emergencyFundRangeVal");
@@ -23,15 +21,16 @@ eFundVal.innerHTML = eFundSlider.value;
 rFundVal.innerHTML = rFundSlider.value;
 // event called when slider is moved
 eFundSlider.oninput = function () {
-  eFundVal.innerHTML = eFundSlider.value;
+  eFundVal.innerHTML = `$${eFundSlider.value}`;
 };
 rFundSlider.oninput = function () {
-  rFundVal.innerHTML = rFundSlider.value;
+  rFundVal.innerHTML = `$${rFundSlider.value}`;
 };
 
 calcBttn.addEventListener("click", () => {
   const monthlyExpense = monthlyExpenseInput.value;
   const monthlyIncome = monthlyIncomeInput.value;
+  console.log(monthlyExpense.value);
 
   //let output = document.querySelector("#error");
   //output.innerHTML = " ";
@@ -42,17 +41,21 @@ calcBttn.addEventListener("click", () => {
     let output = document.querySelector("#error");
     output.innerHTML = " ";
     output = document.querySelector("#netIncome");
-    output.innerHTML = netIncome;
+    output.innerHTML = `$${netIncome}`;
     output = document.querySelector("#netExpenses");
-    output.innerHTML = monthlyExpense;
+    output.innerHTML = `$${monthlyExpense}`;
     output = document.querySelector("#independentTime"); 
-    output.innerHTML = investTime;
+    output.innerHTML = `${investTime} years`;
   
     getEFund(monthlyExpense, netIncome, eFundSlider.value);
     getRFund(monthlyExpense, netIncome, rFundSlider.value);
-  } else {
+  } else if(parseInt(monthlyIncome) <= parseInt(monthlyExpense)) {
     let output = document.querySelector("#error");
     output.innerHTML = "Income must be greater than expense";
+  } 
+  else {
+    let output = document.querySelector("#error");
+    output.innerHTML = "Monthly Income and Monthly Expenses are required fields";
   }
 
 });
@@ -62,9 +65,9 @@ function getEFund(monthlyExpense, netIncome, months) {
   const eFundCap = CalcFundCapacity(monthlyExpense, months);
   const eFundTime = CalculateMonths(eFundCap, netIncome);
   let output = document.querySelector("#eFundCap");
-  output.innerHTML = eFundCap;
+  output.innerHTML = `$${eFundCap}`;
   output = document.querySelector("#eFundTime");
-  output.innerHTML = eFundTime;
+  output.innerHTML = `${eFundTime} month(s)`;
 }
 
 // Display the results for rainy day fund
@@ -72,7 +75,7 @@ function getRFund(monthlyExpense, netIncome, months) {
   const rFundCap = CalcFundCapacity(monthlyExpense, months);
   const rFundTime = CalculateMonths(rFundCap, netIncome);
   let output = document.querySelector("#rFundCap");
-  output.innerHTML = rFundCap;
+  output.innerHTML = `$${rFundCap}`;
   output = document.querySelector("#rFundTime");
-  output.innerHTML = rFundTime;
+  output.innerHTML = `${rFundTime} month(s)`;
 }
